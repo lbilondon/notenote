1c992c84779494755734f934a2cd4f1ebc536c76
unknown <Testing@TestingVM.(none)>
2012-04-27 10:02:07 +0100

    initial commit

---
+++ notenote/ViewModels/NoteViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace notenote
+{
+    public class NoteViewModel : INotifyPropertyChanged
+    {
+        private DateTime _dateCreated;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime DateCreated
+        {
+            get
+            {
+                return _dateCreated;
+            }
+            set
+            {
+                if (value != _dateCreated)
+                {
+                    _dateCreated = value;
+                    NotifyPropertyChanged("DateCreated");
+                }
+            }
+        }
+
+        private DateTime _dateUpdated;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime DateUpdated
+        {
+            get
+            {
+                return _dateUpdated;
+            }
+            set
+            {
+                if (value != _dateUpdated)
+                {
+                    _dateUpdated = value;
+                    NotifyPropertyChanged("DateUpdated");
+                }
+            }
+        }
+
+        private string _title;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (value != _title)
+                {
+                    _title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        private string _body;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string Body
+        {
+            get
+            {
+                return _body;
+            }
+            set
+            {
+                if (value != _body)
+                {
+                    _body = value;
+                    NotifyPropertyChanged("Body");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
